020dee5a4f22f52006a5fe4f3a19bf09ca6edd93
Stafford Rose <staffordrose@gmail.com>
2023-02-27 22:35:14 -0700

    Add delete account method

--- features/user/AccountDetails.tsx
+++ features/user/AccountDetails.tsx
@@ -1,9 +1,19 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { useRouter } from 'next/router';
+import { signOut } from 'next-auth/react';
 
-import { Link, List, ToastContents } from '@/components/atoms';
+import {
+  Button,
+  Dialog,
+  Flex,
+  Link,
+  List,
+  ToastContents,
+} from '@/components/atoms';
 import { styled } from '@/styles';
 import { User } from '@/types';
 import { ProfileImageUploader } from '../uploads';
+import { deleteAccount } from '@/lib/v1/api/user';
 
 type AccountDetails = {
   user: User;
@@ -11,6 +21,10 @@ type AccountDetails = {
 };
 
 export const AccountDetails: FC<AccountDetails> = ({ user, openToast }) => {
+  const router = useRouter();
+
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
   return (
     <StyledAccountDetails>
       <BorderBox
@@ -27,6 +41,40 @@ export const AccountDetails: FC<AccountDetails> = ({ user, openToast }) => {
           </li>
           <li>
             <Link href='/account/username'>Change your username</Link>
+          </li>
+          <li>
+            <Dialog
+              isOpen={isDialogOpen}
+              setOpen={setDialogOpen}
+              title='Are you sure?'
+              body={
+                <Flex direction='column' gap='md'>
+                  <p>
+                    Are you sure you want to delete your account? This action
+                    cannot be undone.
+                  </p>
+                  <Flex justifyContent='end'>
+                    <Button
+                      variant='solid'
+                      size='md'
+                      onClick={() => {
+                        try {
+                          deleteAccount();
+                          signOut();
+                          router.push('/');
+                        } catch (error) {}
+                      }}
+                    >
+                      Confirm
+                    </Button>
+                  </Flex>
+                </Flex>
+              }
+            >
+              <Button aria-label='Open modal to delete account'>
+                Delete your account
+              </Button>
+            </Dialog>
           </li>
         </List>
       </BorderBox>
--- lib/v1/user/deleteAccount.ts
+++ lib/v1/user/deleteAccount.ts
@@ -1,13 +1,133 @@
 import { User } from '@/types';
 import { StatusError } from '@/utils';
+import { getXataClient } from '@/xata';
+import { deleteRoute } from '../routes';
+import { unfavoriteRoute, unfollowUser } from '.';
 
 export const deleteAccount = async (authUserId: User['id']): Promise<void> => {
   try {
-    // TODO: Remove
-    console.log('authUserId', authUserId);
+    const xata = getXataClient();
 
-    // TODO: Create transaction that removes all user-related table rows
-    throw new StatusError(501, `This functionality has not been implemented`);
+    const authUser = await xata.db.users
+      .filter('id', authUserId)
+      .select(['*'])
+      .getFirst();
+
+    if (!authUser) {
+      throw new StatusError(403, `There is no record of your account`);
+    }
+
+    const routes = await xata.db.routes
+      .filter('owner.id', authUserId)
+      .select(['*'])
+      .getAll();
+
+    // delete routes
+    if (Array.isArray(routes) && routes.length) {
+      const routePromises = routes.map((route) =>
+        deleteRoute(authUser, authUser.username, route.slug)
+      );
+
+      await Promise.all(routePromises);
+    }
+
+    const favorites = await xata.db.route_favorites
+      .filter({ user: authUserId })
+      .select(['id', 'route.owner.username', 'route.slug'])
+      .getFirst();
+
+    // delete favorites
+    if (Array.isArray(favorites) && favorites.length) {
+      const favoritePromises = favorites.map((favorite) =>
+        unfavoriteRoute(
+          authUser,
+          favorite.route.owner.username,
+          favorite.route.slug
+        )
+      );
+
+      await Promise.all(favoritePromises);
+    }
+
+    const followerRelationships = await xata.db.user_relationships
+      .filter({ follower: authUserId })
+      .select(['id', 'followed.username'])
+      .getFirst();
+
+    // delete user relationships where auth is follower
+    if (Array.isArray(followerRelationships) && followerRelationships.length) {
+      const relationshipPromises = followerRelationships.map((relationship) =>
+        unfollowUser(authUser, relationship.followed.username)
+      );
+
+      await Promise.all(relationshipPromises);
+    }
+
+    const followedRelationships = await xata.db.user_relationships
+      .filter({ followed: authUserId })
+      .select(['id', 'follower.*'])
+      .getFirst();
+
+    // delete user relationships where auth is followed
+    if (Array.isArray(followedRelationships) && followedRelationships.length) {
+      const relationshipPromises = followedRelationships.map((relationship) =>
+        unfollowUser(relationship.follower, authUser.username)
+      );
+
+      await Promise.all(relationshipPromises);
+    }
+
+    // delete users table row
+    await authUser.delete();
+
+    const [verificationToken, userSession, session, user, account] =
+      await Promise.all([
+        xata.db.nextauth_verificationTokens
+          .filter('id', authUserId)
+          .select(['id'])
+          .getFirst(),
+        xata.db.nextauth_users_sessions
+          .filter('user.id', authUserId)
+          .select(['id'])
+          .getFirst(),
+        xata.db.nextauth_sessions
+          .filter('user.id', authUserId)
+          .select(['id'])
+          .getFirst(),
+        xata.db.nextauth_users
+          .filter('id', authUserId)
+          .select(['id'])
+          .getFirst(),
+        xata.db.nextauth_accounts
+          .filter('user.id', authUserId)
+          .select(['id'])
+          .getFirst(),
+      ]);
+
+    // delete auth verification token
+    if (verificationToken) {
+      await xata.db.nextauth_verificationTokens.delete(verificationToken.id);
+    }
+
+    // delete auth users/sessions join table row
+    if (userSession) {
+      await xata.db.nextauth_users_sessions.delete(userSession.id);
+    }
+
+    // delete auth session
+    if (session) {
+      await xata.db.nextauth_sessions.delete(session.id);
+    }
+
+    // delete auth user
+    if (user) {
+      await xata.db.nextauth_users.delete(user.id);
+    }
+
+    // delete auth account
+    if (account) {
+      await xata.db.nextauth_accounts.delete(account.id);
+    }
   } catch (error) {
     if (error instanceof Error) {
       throw error;
